35c5b2cf8aecc02f15b6cc96683106cf15680129
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-01-28 20:26:59 +0000

    Connect views to urls file

--- about/views.py
+++ about/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def about_me(request):
+    return HttpResponse('This is going to be the about page')
+
--- my_project/urls.py
+++ my_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hello_world import views as index_views
+from about import views as about_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('hello/', index_views.index, name='index'),
+    path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
 ]
