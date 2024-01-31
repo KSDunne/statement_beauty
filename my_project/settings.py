5e35edf64bba9d9c84ebaccc62bba0f2181b592a
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-01-31 18:20:04 +0000

    Add variables to env file

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_@&h1gtte=i$nz%z0pm%#*6)1xd!d+_37)0hg!-$o4+58=-i2g'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
