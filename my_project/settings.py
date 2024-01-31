bfd447fe4c4fbd0c28c10d063425e19a20cab1d6
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-01-31 19:15:42 +0000

    Add a post model to blog app

--- my_project/settings.py
+++ my_project/settings.py
@@ -91,6 +91,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
