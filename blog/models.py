db2c5034b552ee2dcbea34b0b12a99de3026d7f5
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-01 16:22:17 +0000

    Add comment model to blog app

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     
+    
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
