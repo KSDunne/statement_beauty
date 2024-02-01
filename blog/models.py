a55c0be5a9f3f4c23a51cc9fc82d35c0bc5d3ed8
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-01 18:23:04 +0000

    Add methods to the comment model

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+        
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
     
     
 class Comment(models.Model):
@@ -25,3 +31,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+        
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
