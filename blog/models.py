23928d681164d946eac73ba73585dd11f46ff6a1
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-01 10:27:24 +0000

    Add updated on to post model

--- blog/models.py
+++ blog/models.py
@@ -14,4 +14,5 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
     
