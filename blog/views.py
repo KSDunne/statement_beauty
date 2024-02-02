87c7c6740fedb4936c3fc66b6e2ea490a5f56bcb
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-02 16:39:29 +0000

    First step to building home page with Django

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Views
-
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
--- my_project/urls.py
+++ my_project/urls.py
@@ -18,11 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from hello_world import views as index_views
 from about import views as about_views
-from blog import views as blog_views
 
 urlpatterns = [
     path('hello/', index_views.index, name='index'),
     path('about/', about_views.about_me, name='about'),
-    path('blog/', blog_views.my_blog, name='blog'),
+    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
 ]
