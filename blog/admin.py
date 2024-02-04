612a21ff66bb9272eb380e3811f775b64b135d35
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-04 17:35:15 +0000

    Further customisation of admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -6,9 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
+    list_display = ('title', 'slug', 'status','created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status','created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
     
