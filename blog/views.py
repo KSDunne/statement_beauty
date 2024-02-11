8a089bf6b6cea70249a22528b37b9acb90ce4804
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-11 20:11:49 +0000

    Display comments under a blog

--- blog/views.py
+++ blog/views.py
@@ -27,9 +27,15 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {
+          "post": post,
+          "comments": comments,
+          "comment_count": comment_count,
+        },
     )
