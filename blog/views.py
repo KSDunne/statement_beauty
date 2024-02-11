deeb95b8178a99ce3f3434e302af86ad5d7baceb
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-11 20:39:54 +0000

    Leave a comment as an authenticated website user without admin functionality

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
 
 # Views
 
@@ -29,6 +31,20 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -37,5 +53,6 @@ def post_detail(request, slug):
           "post": post,
           "comments": comments,
           "comment_count": comment_count,
+          "comment_form": comment_form,
         },
     )
