d4e8299b0763aa356d9f87cff41b8c59815388d2
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-12 17:48:34 +0000

    Handle collaborate form post request

--- about/views.py
+++ about/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
 
 def about_me(request):
-    """
-    Renders the About page
-    """
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, 
+"Collaboration request received! I try to respond within 2 working days.")
+
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
