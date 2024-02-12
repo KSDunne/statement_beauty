ac72ca39f9c4c9c93cbaacb30b2cb5b56009a477
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-12 16:14:19 +0000

    Add collaboration form to about app

--- about/models.py
+++ about/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# Create your models here.
+
 
 class About(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +10,13 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 
 def about_me(request):
@@ -7,9 +8,13 @@ def about_me(request):
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
     )
