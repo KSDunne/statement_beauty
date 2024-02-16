8160d49c5c70e5953f3feb623ce126863eb195ec
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-16 18:18:30 +0000

    Create a forms python file in the makeover app

--- makeover/views.py
+++ makeover/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Makeover
+from .models import Makeover, Booking
+from .forms import BookingForm
 
 
 def makeover_deals(request):
@@ -7,9 +8,15 @@ def makeover_deals(request):
     Renders the Makeover page
     """
     makeover = Makeover.objects.all().order_by('-updated_on').first()
+    booking = Booking.objects.all().order_by('-date_of_booking').first()
+    booking_form = BookingForm()
 
     return render(
         request,
         "makeover/makeover.html",
-        {"makeover": makeover},
+        {
+         "makeover": makeover,
+         "booking": booking,
+         "booking_form": booking_form,
+         },
     )
