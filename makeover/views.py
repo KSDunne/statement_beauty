b8a319a02705bee42b0de9f22a88b74893f7773e
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-25 16:14:06 +0000

    Display success message string on book makeover page

--- makeover/views.py
+++ makeover/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import Makeover, Booking
 from .forms import BookingForm
 
@@ -8,7 +9,6 @@ def makeover_deals(request):
     Renders the Makeover page
     """
     makeover = Makeover.objects.all().order_by('-updated_on').first()
-    bookings = Booking.objects.all().order_by('-date_of_booking')
     
     if request.method == "POST":
         booking_form = BookingForm(data=request.POST)
@@ -16,8 +16,10 @@ def makeover_deals(request):
             bookings = booking_form.save(commit=False)
             bookings.username = request.user
             bookings.save()
+            messages.add_message(request, messages.SUCCESS, 
+"Booking request received! Please await confirmation.")
 
-    
+    bookings = Booking.objects.all().order_by('-date_of_booking')
     booking_form = BookingForm()
 
 
