047d21e31ab92f39bdfef477ce7ebbdf543dd6f6
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-02-26 20:10:38 +0000

    Collect static now that update booking is almost working

--- staticfiles/js/bookings.js
+++ staticfiles/js/bookings.js
@@ -1,14 +1,16 @@
-const editButtons = document.getElementsByClassName("btn-edit-book");
-const bookingText = document.getElementById("id_message");
-const bookingForm = document.getElementById("bookingForm");
-const submitButton = document.getElementById("submitButtonBook");
+document.addEventListener("DOMContentLoaded", function () {
+  const editButtons = document.querySelectorAll(".btn-edit-book");
+  const bookingText = document.getElementById("id_message");
+  const bookingForm = document.getElementById("bookingForm");
+  const submitButton = document.getElementById("submitButtonBook");
 
-for (let button of editButtons) {
-  button.addEventListener("click", (e) => {
-    let bookingId = e.target.getAttribute("booking_id");
-    let bookingContent = document.getElementsByTagName("h5").innerText;
-    bookingText.value = bookingContent;
-    submitButton.innerText = "Update";
-    bookingForm.setAttribute("action", `booking_edit/${bookingId}`);
+  editButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      let bookingId = e.target.getAttribute("data-booking_id");
+      let bookingContent = document.getElementById(`booking${bookingId}`);
+      bookingText.value = bookingContent.innerText;
+      submitButton.innerText = "Update";
+      bookingForm.setAttribute("action", `booking_edit/${bookingId}`);
+    });
   });
-}
+});
