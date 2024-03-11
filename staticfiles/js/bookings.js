677e56777cb338d20e4b0a51b88344fc974300a4
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-03-11 18:10:09 +0000

    Add comments to booking script

--- staticfiles/js/bookings.js
+++ staticfiles/js/bookings.js
@@ -1,12 +1,19 @@
+// JS script with event listeners for editing and deleting a makeover booking
+// Note: A defensive design was built in to the delete funtion with a modal
+
+// constants for edit event listener
 const editButtons = document.querySelectorAll(".btn-edit");
 const bookingText = document.getElementById("id_message");
 const bookingForm = document.getElementById("booking-form");
 const submitButton = document.getElementById("submit-buttonbook");
 
+// constants for delete event listener
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.querySelectorAll(".btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
+// here is editing functionality which fills the message box with details to be edited
+// this shows the user they are editing the intended booking
 editButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     let bookingId = e.target.getAttribute("data-booking_id");
@@ -18,6 +25,8 @@ editButtons.forEach((button) => {
   });
 });
 
+// here is the code which enables the deletion functionality
+// this prompts the user to confirm they really want to delete a booking
 deleteButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     let bookingId = e.target.getAttribute("data-booking_id");
