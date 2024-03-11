08cade80091e13d1931423ddebb14191154eb545
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-03-11 17:57:38 +0000

    Remove dom content loaded from booking js as this was for debugging

--- staticfiles/js/bookings.js
+++ staticfiles/js/bookings.js
@@ -1,31 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const editButtons = document.querySelectorAll(".btn-edit");
-  const bookingText = document.getElementById("id_message");
-  const bookingForm = document.getElementById("booking-form");
-  const submitButton = document.getElementById("submit-buttonbook");
+const editButtons = document.querySelectorAll(".btn-edit");
+const bookingText = document.getElementById("id_message");
+const bookingForm = document.getElementById("booking-form");
+const submitButton = document.getElementById("submit-buttonbook");
 
-  const deleteModal = new bootstrap.Modal(
-    document.getElementById("deleteModal")
-  );
-  const deleteButtons = document.querySelectorAll(".btn-delete");
-  const deleteConfirm = document.getElementById("deleteConfirm");
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.querySelectorAll(".btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
 
-  editButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      let bookingId = e.target.getAttribute("data-booking_id");
-      let bookingContent = document.getElementById(`booking${bookingId}`);
-      let message = bookingContent.querySelector(".booking-message").innerText;
-      bookingText.value = message;
-      submitButton.innerText = "Update";
-      bookingForm.setAttribute("action", `booking_edit/${bookingId}`);
-    });
+editButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let bookingId = e.target.getAttribute("data-booking_id");
+    let bookingContent = document.getElementById(`booking${bookingId}`);
+    let message = bookingContent.querySelector(".booking-message").innerText;
+    bookingText.value = message;
+    submitButton.innerText = "Update";
+    bookingForm.setAttribute("action", `booking_edit/${bookingId}`);
   });
+});
 
-  deleteButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      let bookingId = e.target.getAttribute("data-booking_id");
-      deleteConfirm.href = `delete_booking/${bookingId}`;
-      deleteModal.show();
-    });
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let bookingId = e.target.getAttribute("data-booking_id");
+    deleteConfirm.href = `delete_booking/${bookingId}`;
+    deleteModal.show();
   });
 });
