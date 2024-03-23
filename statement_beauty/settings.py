1115ddfebc475b58cf8a96d8e23487a845301661
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-03-23 21:00:03 +0000

    Remove commented out code from settings.py

--- statement_beauty/settings.py
+++ statement_beauty/settings.py
@@ -102,13 +102,6 @@ WSGI_APPLICATION = "statement_beauty.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 CSRF_TRUSTED_ORIGINS = ["https://*.codeanyapp.com", "https://*.herokuapp.com"]
